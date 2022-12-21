7750163eb841fa2004bdd34685ad21e420a6747b
prgtyfzh <pragitya.faazha.ft20@mail.umy.ac.id>
2022-12-21 10:35:19 +0700

    second commit

---
+++ database/seeders/MahasiswaSeeder.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use App\Models\Mahasiswa;
+use Faker\Factory as Faker;
+
+class MahasiswaSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $faker= Faker::create('id_ID');
+
+        for($i = 1; $i <= 20; $i++) {
+            Mahasiswa::create([
+                'nama' => $faker->name,
+                'nim' => $faker->buildingNumber,
+                'alamat' => $faker->address
+            ]);
+        }
+    }
+}
